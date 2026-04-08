957c34711c1d4ac616e573c3aca06e85f6f76e22
MaycolYahir <maycolmezapadilla@gmail.com>
2026-04-07 23:29:34 -0400

    used copilot to create some test, and setup the graddle wrapper

--- backend/src/test/java/com/bloc/app/BlocApplicationTests.java
+++ backend/src/test/java/com/bloc/app/BlocApplicationTests.java
@@ -2,9 +2,19 @@ package com.bloc.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 
-@SpringBootTest
+import com.bloc.app.repository.SidequestRepository;
+
+@SpringBootTest(properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration")
 class BlocApplicationTests {
+
+    @MockBean
+    private SidequestRepository sidequestRepository;
+
+    @MockBean
+    private JwtDecoder jwtDecoder;
 
     @Test
     void contextLoads() {
