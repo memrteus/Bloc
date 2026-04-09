37772a14036f692e87e99d7d4556941bcf961823
MaycolYahir <maycolmezapadilla@gmail.com>
2026-04-08 22:26:39 -0400

    Stop tracking environment.ts

--- frontend/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  apiBaseUrl: 'http://localhost:8080/api',
-  supabaseUrl: 'db.juobtozbimgjeothtchz.supabase.co',
-  supabasePublishableKey: 'sb_publishable_WvXP1ATjETTEuX9nPY9Rtw_KUyKbtEF'
-};
